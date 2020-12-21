8320e510846befa4723a2c994adef723799df699
ftsyntrax <kapito@mail.bg>
2020-12-21 20:35:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
+
+const App = () => {
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Find your Recipe</h1>
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,21 @@
+import style from './recipe.module.css';
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={style.recipe}>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+
+      <p>
+        <strong>Калории:</strong> {calories.toFixed(2)} kcal
+      </p>
+      <img className={style.image} src={image} alt="" />
+    </div>
+  );
+};
+
+export default Recipe;
