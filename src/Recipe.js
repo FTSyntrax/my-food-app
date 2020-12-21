8f96e51a93bedf52bcc456d0f143e1fcbef73b45
ftsyntrax <kapito@mail.bg>
2020-12-21 22:26:16 +0200

    More styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,7 +3,7 @@ import style from './recipe.module.css';
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <ol>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
